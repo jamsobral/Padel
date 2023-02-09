a33c847d83db32d50306f552b2900123fdc22b98
jamsobral <43965691+jamsobral@users.noreply.github.com>
2023-02-09 10:31:04 +0000

    first commit teste

--- shared/src/iosMain/kotlin/com/example/padel/Platform.kt
+++ shared/src/iosMain/kotlin/com/example/padel/Platform.kt
@@ -4,6 +4,8 @@ import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+
+
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
